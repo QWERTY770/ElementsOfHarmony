c82ee5f10b64ac31f3b29446527bbffbb61e4dcb
Twilight Sparkle <twilight_princess_sparkle@outlook.com>
2020-06-01 15:32:07 +0800

    Correct MisWritten MOD ID

--- src/main/java/top/xuogroup/mcmods/elementsofharmony/DimensionRegistry.java
+++ src/main/java/top/xuogroup/mcmods/elementsofharmony/DimensionRegistry.java
@@ -6,6 +6,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class DimensionRegistry {
-    public static final DeferredRegister<ModDimension> MOD_DIMENSION = new DeferredRegister<>(ForgeRegistries.MOD_DIMENSIONS, "equestria");
+    public static final DeferredRegister<ModDimension> MOD_DIMENSION = new DeferredRegister<>(ForgeRegistries.MOD_DIMENSIONS, "elementsofharmony");
     public static RegistryObject<ElementsOfHarmonyDimensions> equestriaDimension = MOD_DIMENSION.register("equestria_dimension", ElementsOfHarmonyDimensions::new);
 }
