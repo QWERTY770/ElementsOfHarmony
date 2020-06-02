30436d580241e5a027c0b1f862a82a1bedef005a
Twilight Sparkle <twilight_princess_sparkle@outlook.com>
2020-06-02 13:10:37 +0800

    Dispose Failed Dimension (Try Again Later)

--- src/main/java/top/xuogroup/mcmods/elementsofharmony/ElementsOfHarmony.java
+++ src/main/java/top/xuogroup/mcmods/elementsofharmony/ElementsOfHarmony.java
@@ -6,11 +6,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("elementsofharmony")
 public class ElementsOfHarmony {
-
     public ElementsOfHarmony() {
         ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
         BlockRegistry.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        DimensionRegistry.MOD_DIMENSION.register(FMLJavaModLoadingContext.get().getModEventBus());
         // Set Up Registry
     }
 }
--- src/main/java/top/xuogroup/mcmods/elementsofharmony/ItemEEquestriaTransferAmulet.java
+++ src/main/java/top/xuogroup/mcmods/elementsofharmony/ItemEEquestriaTransferAmulet.java
@@ -1,13 +1,11 @@
 package top.xuogroup.mcmods.elementsofharmony;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.EggEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 
@@ -18,17 +16,5 @@ import java.util.Objects;
 public class ItemEEquestriaTransferAmulet extends Item {
     public ItemEEquestriaTransferAmulet(){
         super(new Properties().group(ModGroup.elementsofharmonyGroup));
-    }
-    @Nonnull @ParametersAreNonnullByDefault
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-        ItemStack itemstack = playerIn.getHeldItem(handIn);
-        playerIn.addStat(Stats.ITEM_USED.get(this));
-        if (!playerIn.abilities.isCreativeMode) {
-            itemstack.shrink(1);
-        }
-        if (!worldIn.isRemote) {
-            playerIn.changeDimension(Objects.requireNonNull(DimensionType.getById(2)));
-        }
-        return ActionResult.resultSuccess(itemstack);
     }
 }
