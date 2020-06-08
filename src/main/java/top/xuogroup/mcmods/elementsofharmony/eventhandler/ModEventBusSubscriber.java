38d8c80444ef96d25303e9808f58285aa86ed384
Twilight Sparkle <Twilight_Princess_Sparkle@outlook.com>
2020-06-08 16:10:44 +0800

    Remove addSpawnBiome

--- src/main/java/top/xuogroup/mcmods/elementsofharmony/eventhandler/ModEventBusSubscriber.java
+++ src/main/java/top/xuogroup/mcmods/elementsofharmony/eventhandler/ModEventBusSubscriber.java
@@ -15,10 +15,7 @@ public abstract class ModEventBusSubscriber {
     @SubscribeEvent
     public static void onSetUpEvent(FMLCommonSetupEvent event){
         BiomeManager.addBiome(BiomeManager.BiomeType.COOL, new BiomeManager.BiomeEntry(BiomeRegistry.biomeEEquestriaForest.get(), 800));
-        BiomeManager.addSpawnBiome(BiomeRegistry.biomeEEquestriaForest.get());
-
         BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(BiomeRegistry.biomeEEquestriaPlains.get(), 1200));
-        BiomeManager.addSpawnBiome(BiomeRegistry.biomeEEquestriaPlains.get());
         // Add Biomes
 
         // Sep ---------------------------------------------------------------------------------------------------------
